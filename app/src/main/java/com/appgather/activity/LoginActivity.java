7c54f9ad3bc43198844b3399e46e25202664e8ce
Hua <1057918521@qq.com>
2016-11-23 23:16:51 +0800

    init1.1

--- app/src/main/java/com/appgather/activity/LoginActivity.java
+++ app/src/main/java/com/appgather/activity/LoginActivity.java
@@ -44,6 +44,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.login);
         getSupportActionBar().hide();
+        initView();
+    }
+
+    private void initView() {
         et_loginpassword = (EditText) findViewById(R.id.et_loginpassword);
         et_loginname = (EditText) findViewById(R.id.et_loginname);
         tv_blank = (TextView) findViewById(R.id.tv_blank);
@@ -51,13 +55,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         btn_register=(Button)findViewById(R.id.btn_forregister);
         btn_canclename=(Button)findViewById(R.id.btn_canclename);
         btn_canclepassword=(Button)findViewById(R.id.btn_canclepassword);
-
         btnListen();//按钮事件监听
         editviewFocus();//编辑框焦点事件监听
         inputModeListen();//软键盘弹出监听
         editViewListenInput();//编辑框输入监听
-
-
     }
 
     //按钮监听器
@@ -144,61 +145,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-
-    /*
-    * 监听点击事件   dispatch事件分发
-    * */
-    /*
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
-            View v = getCurrentFocus();
-            if (isShouldHideKeyboard(v, ev)) {
-                hideKeyboard(v.getWindowToken());
-            }
-        }
-        return super.dispatchTouchEvent(ev);
-    }
-
-    /**
-     * 根据EditText所在坐标和用户点击的坐标相对比，来判断是否隐藏键盘，因为当用户点击EditText时则不能隐藏
-     *
-     * @param v
-     * @param event
-     * @return
-     */
-    private boolean isShouldHideKeyboard(View v, MotionEvent event) {
-
-        if (v != null && (v instanceof EditText)) {
-            int[] l = {0, 0};
-            v.getLocationInWindow(l);
-            int left = l[0],
-                    top = l[1],
-                    bottom = top + v.getHeight(),
-                    right = left + v.getWidth();
-            if (event.getX() > left && event.getX() < right
-                    && event.getY() > top && event.getY() < bottom) {
-                // 点击EditText的事件，忽略它。
-                return false;
-            } else {
-                return true;
-            }
-        }
-        // 如果焦点不是EditText则忽略，这个发生在视图刚绘制完，第一个焦点不在EditText上，和用户用轨迹球选择其他的焦点
-        return false;
-    }
-
-    /**
-     * 获取InputMethodManager，隐藏软键盘
-     * @param token
-     */
-    private void hideKeyboard(IBinder token) {
-        if (token != null) {
-            InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            im.hideSoftInputFromWindow(token, InputMethodManager.HIDE_NOT_ALWAYS);
-        }
-    }
-
     @Override
     public void onClick(View view) {
         switch (view.getId()){
@@ -206,7 +152,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
                 startActivity(intent);break;
             case R.id.btn_login:
-                Intent intent2=new Intent(LoginActivity.this,MainViewActivity.class);
+                Intent intent2=new Intent(LoginActivity.this,MainInterfaceActivity.class);
                 startActivity(intent2);
                 break;
             case R.id.btn_canclename:
